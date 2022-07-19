86ef55e251a816b1ba1eaf13ed825e292b5aa135
Emanuel Hein <emanuel.hein@code-better.it>
2022-07-19 16:43:34 +0200

    pre deployed templates

---
+++ server/rest/templates/f73c0653-61e7-40ae-8429-aad3a903c830/tpl-work-report-header.entry.js
@@ -0,0 +1,15 @@
+import { r as registerInstance, h, H as Host } from './index-63403316.js';
+
+const _0WorkReportHeaderCss = "body{font-size:14pt;font-family:\"Lucida Sans\", \"Lucida Sans Regular\", \"Lucida Grande\", \"Lucida Sans Unicode\", Geneva, Verdana, sans-serif !important}tpl-work-report-header{display:block;min-height:3cm;box-sizing:border-box;border:1mm solid purple;padding:1cm}";
+
+const WorkReportHeaderTpl = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  render() {
+    return h(Host, null, "work report header");
+  }
+};
+WorkReportHeaderTpl.style = _0WorkReportHeaderCss;
+
+export { WorkReportHeaderTpl as tpl_work_report_header };
