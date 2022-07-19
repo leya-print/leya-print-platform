429b2b0514d8e8790f8df0ded53814e7d6618311
Emanuel Hein <emanuel.hein@code-better.it>
2022-07-19 16:43:34 +0200

    pre deployed templates

---
+++ server/rest/templates/f01155b1-7ac8-4cec-a493-cc618e56055e/index.js
@@ -0,0 +1,29 @@
+import { i as invoiceSamples } from './invoice-samples-ab010f4d.js';
+
+const invoiceTemplateInfo = {
+  ident: 'invoice',
+  title: 'Invoice',
+  description: 'invoices for customers',
+  components: {
+    header: 'tpl-invoice-header',
+    content: 'tpl-invoice-content',
+    footer: 'tpl-invoice-footer',
+  },
+  sampleData: {
+    'invoice-001': {
+      title: 'invoice 0001',
+      description: 'simple sample of an invoice',
+      data: invoiceSamples['invoice-001'],
+    },
+  },
+};
+
+const templatePackage = {
+  ident: 'lenas-templates',
+  version: '1.0.0',
+  templates: [
+    invoiceTemplateInfo,
+  ],
+};
+
+export { templatePackage };
---
+++ server/rest/templates/f73c0653-61e7-40ae-8429-aad3a903c830/loader.js
@@ -0,0 +1,17 @@
+import { p as promiseResolve, b as bootstrapLazy } from './index-63403316.js';
+
+/*
+ Stencil Client Patch Esm v2.17.1 | MIT Licensed | https://stenciljs.com
+ */
+const patchEsm = () => {
+    return promiseResolve();
+};
+
+const defineCustomElements = (win, options) => {
+  if (typeof window === 'undefined') return Promise.resolve();
+  return patchEsm().then(() => {
+  return bootstrapLazy([["tpl-invoice-header",[[0,"tpl-invoice-header",{"invoice":[32]}]]],["my-component",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]],["tpl-invoice-content",[[0,"tpl-invoice-content",{"invoice":[32]}]]],["tpl-invoice-footer",[[0,"tpl-invoice-footer",{"invoice":[32]}]]],["tpl-template-demo-content",[[0,"tpl-template-demo-content"]]],["tpl-template-demo-footer",[[0,"tpl-template-demo-footer"]]],["tpl-template-demo-header",[[0,"tpl-template-demo-header"]]],["tpl-work-report-content",[[0,"tpl-work-report-content"]]],["tpl-work-report-footer",[[0,"tpl-work-report-footer"]]],["tpl-work-report-header",[[0,"tpl-work-report-header"]]],["tplb-address_2",[[0,"tplb-address",{"address":[16]}],[0,"tplb-address-line",{"address":[16]}]]]], options);
+  });
+};
+
+export { defineCustomElements };
