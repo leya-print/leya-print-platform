cb798aff43283a544205bc2e9517c1378902d5b0
Emanuel Hein <gitlab@e-hein.de>
2023-04-07 07:31:03 +0000

    parallel build of frontend and backend

--- build/scripts/build-all.sh
+++ build/scripts/build-all.sh
@@ -3,6 +3,13 @@ set -e
 . $(dirname $0)/env.sh
 
 LB_BUILD_SCRIPT_DIR=$LB_WORKSPACE_ROOT/build/scripts
-. $LB_BUILD_SCRIPT_DIR/build-web-designer.sh
-. $LB_BUILD_SCRIPT_DIR/build-server-rest.sh
+
+# Build web frontend and rest server in parallel
+(. $LB_BUILD_SCRIPT_DIR/build-web-designer.sh) & \
+(. $LB_BUILD_SCRIPT_DIR/build-server-rest.sh) &
+
+# Wait for both builds to complete
+wait
+
+# Build the Docker image with those results
 . $LB_BUILD_SCRIPT_DIR/build-server-container.sh
