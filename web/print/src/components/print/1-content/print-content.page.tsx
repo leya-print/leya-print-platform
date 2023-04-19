828cedcf09769b8235236f486fc8ce622aa7e6d0
Radu Patruica <patruica.radu@gmail.com>
2023-04-19 20:08:25 +0000

    added print project

---
+++ web/print/src/components/print/1-content/print-content.page.tsx
@@ -0,0 +1,20 @@
+import { Component, h, Host, Prop } from '@stencil/core';
+import { templatePackageService } from 'src/global/template-package.service';
+
+@Component({
+  tag: 'print-content-page',
+  styleUrl: 'print-content.page.scss',
+  shadow: false,
+})
+export class PrintContentPage {
+  @Prop() tplName: string;
+  @Prop() tplPackage?: string;
+
+  async componentWillLoad() {
+    await templatePackageService.defineCustomElements(this.tplPackage);
+  }
+
+  render() {
+    return <Host innerHTML={`<graph-paper></graph-paper><tpl-${this.tplName}-content></tpl-${this.tplName}-content>`}></Host>
+  }
+}
