7fc44ec3182e7184a88f177ab9e5e293056d4ebb
Radu Patruica <patruica.radu@gmail.com>
2023-05-12 20:50:03 +0000

    updates from axios get to cross fetch

--- common/api/index.ts
+++ common/api/index.ts
@@ -1,4 +1,5 @@
 export * from './template-info.model';
 export * from './template-package.model';
 export * from './utils';
+export { fetchWithTimeout } from './utils';
 export * from './declarations';
