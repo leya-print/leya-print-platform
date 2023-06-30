d5df11194df89f1d401fd75d1f965afd92b1803b
Radu Patruica <patruica.radu@gmail.com>
2023-06-30 14:58:12 +0000

    changes to leya common

--- build/scripts/build-all.sh
+++ build/scripts/build-all.sh
@@ -5,12 +5,12 @@ set -e
 LB_BUILD_SCRIPT_DIR=$LB_WORKSPACE_ROOT/build/scripts
 
 # Build web frontend and rest server in parallel
+(. $LB_BUILD_SCRIPT_DIR/build-common.sh) & \
 (. $LB_BUILD_SCRIPT_DIR/build-web-designer.sh) & \
 (. $LB_BUILD_SCRIPT_DIR/build-web-print.sh) & \
 (. $LB_BUILD_SCRIPT_DIR/build-auth-service.sh) & \
 (. $LB_BUILD_SCRIPT_DIR/build-pdf-service.sh) & \
-(. $LB_BUILD_SCRIPT_DIR/build-tpl-service.sh) & \
-(. $LB_BUILD_SCRIPT_DIR/build-common.sh) &
+(. $LB_BUILD_SCRIPT_DIR/build-tpl-service.sh) &
 
 # Wait for both builds to complete
 wait
--- server/container/build.sh
+++ server/container/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker buildx build \
+  --build-context common-api=../../common/api \
   --build-context pdf-service=../pdf-service \
   --build-context tpl-service=../tpl-service \
   --build-context auth-service=../auth-service \
   --build-context web-designer=../../web/designer \
-  --build-context web-print=../../web/print \
-  --build-context common=../common \
+  --build-context web-print=../../web/print \  
   -t leya:local \
   .
