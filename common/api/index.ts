924af7478e69109357b19dd3591bb34565798952
Radu Patruica <patruica.radu@gmail.com>
2023-07-22 14:04:24 +0000

    added rollup

--- common/api/index.ts
+++ common/api/index.ts
@@ -1,7 +1,13 @@
-export * from './certificate-info.model';
-export * from './env.model';
-export * from './service-params.model';
-export * from './template-info.model';
-export * from './template-package.model';
-export * from './utils';
+export * from '/workspace/leya-print/common/api/certificate-info.model';
+export * from '/workspace/leya-print/common/api/service-params.model';
+export * from '/workspace/leya-print/common/api/template-info.model';
+export * from '/workspace/leya-print/common/api/template-package.model';
+export * from '/workspace/leya-print/common/api/utils';
+export * from '/workspace/leya-print/common/api/env.model';
 
+// export * from './certificate-info.model';
+// export * from './service-params.model';
+// export * from './template-info.model';
+// export * from './template-package.model';
+// export * from './utils';
+// export * from './env.model';
