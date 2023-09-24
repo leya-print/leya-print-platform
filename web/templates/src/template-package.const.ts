c6c414cba5d076f07ed030c0d261ab813c873a43
Emanuel Hein <emanuel.hein@code-better.it>
2023-09-24 18:32:07 +0200

    use template api in web templates

--- web/templates/src/template-package.const.ts
+++ web/templates/src/template-package.const.ts
@@ -1,4 +1,4 @@
-import { TemplatePackage } from '@leya-print/common-api';
+import { TemplatePackage } from '@leya-print/template-api';
 import { invoiceTemplateInfo } from './components/templates/invoice/invoice-template-info.const';
 import { templateDemoTemplateInfo } from './components/templates/template-demo/template-demo-template-info.const';
 import { workReportTemplateInfo } from './components/templates/work-report/work-report-template-info.const';
