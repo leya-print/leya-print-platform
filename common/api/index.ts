2c3054d88fcd02ef459985973c0f03fb20ab4cdb
Emanuel Hein <emanuel.hein@code-better.it>
2023-09-24 18:41:36 +0200

    use template api in common api

--- common/api/index.ts
+++ common/api/index.ts
@@ -1,6 +1,5 @@
 export * from './certificate-info.model';
 export * from './service-params.model';
-export * from './template-info.model';
-export * from './template-package.model';
+export * from '@leya-print/template-api';
 export * from './utils';
 export * from './env.model';
