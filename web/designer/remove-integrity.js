b8e128052d32e1be24cad2f1e980491892c7e15b
Radu Patruica <patruica.radu@gmail.com>
2023-10-25 20:21:00 +0000

    script to remove integrity from designer common

---
+++ web/designer/remove-integrity.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const packagesToRemoveIntegrity = process.argv.slice(2);
+
+fs.readFile('package-lock.json', 'utf8', (err, data) => {
+  if (err) throw err;
+
+  const modifiedData = packagesToRemoveIntegrity.reduce((acc, packageName) => {
+    const regex = new RegExp(`"${packageName}"\\s*:\\s*{[^}]*"integrity":\\s*"[^"]*"[^}]*`, 'g');
+    return acc.replace(regex, (match) => match.replace(/"integrity":\s*"[^"]*",/, ''));
+  }, data);
+
+  fs.writeFile('package-lock.json', modifiedData, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('integrity values removed for specified packages');
+  });
+});
