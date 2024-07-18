2c44380f4e8b6f6d7df4dddb8229d3604170d381
Raduvi <patruica.radu@gmail.com>
2024-07-18 11:25:57 +0300

    rm dep itself -> web-common

--- build/scripts/build-web-common.sh
+++ build/scripts/build-web-common.sh
@@ -7,16 +7,3 @@ cd $LB_WORKSPACE_ROOT/web/common
 npm ci
 npm run build
 npm pack
-
-TARBALL_PATH="$LB_WORKSPACE_ROOT/web/common/leya-print-web-common-1.0.0.tgz"
-
-if [ ! -f "$TARBALL_PATH" ]; then
-    echo "Tarball file does not exist: $TARBALL_PATH"
-    exit 1
-fi
-
-tar -tzf "$TARBALL_PATH"
-if [ $? -ne 0 ]; then
-    echo "Tarball file is corrupted: $TARBALL_PATH"
-    exit 1
-fi
