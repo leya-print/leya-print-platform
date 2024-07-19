11e9ae8f565e68cef9341b394158a9f5f6c0cdbd
Raduvi <patruica.radu@gmail.com>
2024-07-19 14:11:59 +0300

    white-paper-template added

---
+++ web/templates/src/components/templates/white-paper/2-white-paper-footer.tsx
@@ -0,0 +1,11 @@
+import { Component, h, Host } from '@stencil/core';
+
+@Component({
+  tag: 'tpl-white-paper-footer',
+  styleUrl: '2-white-paper-footer.scss',
+})
+export class WhitePaperFooterTpl {
+  render() {
+    return <Host>White Paper footer</Host>
+  }
+}
